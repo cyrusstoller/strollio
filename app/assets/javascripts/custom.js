de7c7d4b6af9efa24c2009f6fa8daa202adc1158
Max Schultz <max.schultz@gmail.com>
2012-06-02 23:29:52 -0700

    gallery

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,4 +2,30 @@ $(function(){
 	$("label[data-error]").parent().addClass("error");
 	$("input[data-error]").parent().addClass("error");
 	$("textarea[data-error]").parent().addClass("error");
+
+});
+
+
+
+    $(function(){
+    // Toggle fullscreen button:
+    $('#toggle-fullscreen').button().click(function () {
+        var button = $(this),
+            root = document.documentElement;
+        if (!button.hasClass('active')) {
+            $('#modal-gallery').addClass('modal-fullscreen');
+            if (root.webkitRequestFullScreen) {
+                root.webkitRequestFullScreen(
+                    window.Element.ALLOW_KEYBOARD_INPUT
+                );
+            } else if (root.mozRequestFullScreen) {
+                root.mozRequestFullScreen();
+            }
+        } else {
+            $('#modal-gallery').removeClass('modal-fullscreen');
+            (document.webkitCancelFullScreen ||
+                document.mozCancelFullScreen ||
+                $.noop).apply(document);
+        }
+    });
 });
